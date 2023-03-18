a3bf8a46e99d2ff004382b156af2f8998cbbc5e9
paweljss <jasnastronasieci@gmail.com>
2023-03-18 21:29:09 +0100

    html beta-structure for js. selecting dt thru input

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const searchBar = document.querySelector("[type=search]");
+
+const definitionList = document.querySelectorAll("dt");
+
+const searchDefinition = function (e_target_value) {
+	const regex = new RegExp("^" + e_target_value, "i");
+	let arrOfMatch = [];
+	for (const el of definitionList) {
+		if (regex.test(el.innerHTML)) {
+			arrOfMatch.push(el.innerHTML);
+		}
+	}
+	return arrOfMatch;
+};
+
+searchBar.addEventListener("input", (e) =>
+	console.log(searchDefinition(e.target.value))
+);
